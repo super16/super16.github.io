e32b37646d112801cd96c68379ae042316d5b365
Mark <super16mail@gmail.com>
2023-02-03 18:44:26 +0300

    Refactor header & add ARIA-attributes to menu button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 function toggle() {
   const navBarTag = document.getElementById('toggle-nav-bar');
+  const toggleButton = document.getElementById('toggle-button');
   if (navBarTag.classList.length === 0) {
     navBarTag.classList.add('responsive');
+    toggleButton.setAttribute("aria-expanded", true);
   } else {
     navBarTag.classList.remove('responsive');
+    toggleButton.setAttribute("aria-expanded", false);
   }
 }
